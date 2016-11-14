a0334337d81389d2da5734ffd4e8823a10172f9e
João Eduardo Montandon <edu.montandon@gmail.com>
2016-11-14 17:43:51 -0200

    Implemented A* pathfinding algorithm.

--- aquickstar.js
+++ aquickstar.js
@@ -1,4 +1,5 @@
-var QuadTree = require('./Quadtree');
+var QuadTree = require('./Quadtree'),
+	Heap = require('heap');
 
 
 function AQuickStar(opt) {
@@ -37,6 +38,86 @@ AQuickStar.prototype.removeObstacle = function(x, y) {
 };
 
 
+AQuickStar.prototype.findPath = function(start, end, heuristic) {
+
+	start = {x: start[0], y: start[1], width: 1, height: 1};
+	end = {x: end[0], y: end[1], width: 1, height: 1};
+	heuristic = heuristic || this.manhattanHeuristic;
+
+	this.qt.insert(start);
+	this.qt.insert(end);
+
+	var path = this._findPath(start, end, heuristic);
+
+	this.qt.removeObject(start);
+	this.qt.removeObject(end);
+
+	return path;
+};
+
+/*
+ * Algorithm based on http://www.briangrinstead.com/blog/astar-search-algorithm-in-javascript
+ */
+AQuickStar.prototype._findPath = function(start, end, heuristic) {
+	var sameNodeFn = function(n1, n2) {
+				return n1.x === n2.x && n1.y === n2.y && n1.width === n2.width && n1.height === n2.height;
+	};
+	var openList = new Heap(function(x, y) { return x.f - y.f; });
+	var closedList = [];
+
+	openList.push(start);
+
+	while(!openList.empty()) {
+		currentNode = openList.pop();
+
+		if(sameNodeFn(currentNode, end)) {
+			var curr = currentNode;
+			var path = [];
+			while(curr.parent) {
+				path.push(curr);
+				curr = curr.parent;
+			}
+			path.push(curr);	// push first node (start)
+			return path.reverse();
+		}
+		
+		closedList.push(currentNode);
+		
+		var neighbors = this._getNeighbors(currentNode);
+		for (var ni = 0; ni < neighbors.length; ni++) {
+			var neighbor = neighbors[ni];
+
+			var gScore = (currentNode.g || 0) + neighbor.cost;
+			var neighborIsInArrayFn = function(n) { return sameNodeFn(n, neighbor);	};
+
+			if(!closedList.find(neighborIsInArrayFn)) {
+				var neighborNode = openList.toArray().find(neighborIsInArrayFn);
+				if(!neighborNode) {
+					neighborNode = neighbor;
+					neighborNode.g = gScore;
+					neighborNode.f = gScore + heuristic(end.x, end.y, 
+														neighborNode.x, neighborNode.y);
+					neighborNode.parent = currentNode;
+
+					openList.push(neighborNode);
+				} else if(neighborNode.g > gScore) {
+					neighborNode.g = gScore;
+					neighborNode.f = gScore + heuristic(end.x, end.y, 
+														neighborNode.x, neighborNode.y);
+					neighborNode.parent = currentNode;												
+				}
+			}
+		}
+	}
+	return [];
+};
+
+
+AQuickStar.prototype.manhattanHeuristic = function(x1, y1, x2, y2) {
+	return Math.abs(x2 - x1) + Math.abs(y2 - y1);
+};
+
+
 AQuickStar.prototype._getNeighbors = function(bounds) {
 	var result = [];
 	var neighbors = [];
@@ -69,9 +150,7 @@ AQuickStar.prototype._getNeighbors = function(bounds) {
 	}
 
 	// eliminates duplicated elements
-	result = result.filter((e1, index, self) => 
-									self.findIndex((e2) => 
-											{return e2.y === e1.y && e2.x === e1.x && e2.value === e1.value; }) === index);
+	result = result.filter((e1, index, self) => self.findIndex((e2) => {return e2.y === e1.y && e2.x === e1.x && e2.cost === e1.cost; }) === index);
 
 
 	return result;
@@ -83,13 +162,15 @@ AQuickStar.prototype._getNeighbor = function(node) {
 	var retrievedNeighbor = this.qt.getObjectNode(node);
 
 	// if the node is on the lowest level or it is inside a quadrant with only one block,
-	// then return it with one value
+	// then return it with node cost = 1
 	if (retrievedNeighbor.level == Math.log2(this.width) || 
 		(retrievedNeighbor.level < Math.log2(this.width) && retrievedNeighbor.objects.length)) {
 		return {
 			x: node.x,
 			y: node.y,
-			value: 1
+			width: 1,
+			height: 1,
+			cost: 1
 		};
 	}
 
@@ -97,13 +178,11 @@ AQuickStar.prototype._getNeighbor = function(node) {
 	return {
 		x: retrievedNeighbor.bounds.x,
 		y: retrievedNeighbor.bounds.y,
-		value: Math.sqrt(Math.pow(retrievedNeighbor.bounds.width, 2) + Math.pow(retrievedNeighbor.bounds.height, 2))
+		width: retrievedNeighbor.bounds.width,
+		height: retrievedNeighbor.bounds.height,
+		cost: Math.sqrt(Math.pow(retrievedNeighbor.bounds.width, 2) + Math.pow(retrievedNeighbor.bounds.height, 2))
 	};
 };
 
-
-AQuickStar.prototype.findPath = function(start, end) {
-	console.log("FIND PATH");
-};
 
 module.exports = AQuickStar;
